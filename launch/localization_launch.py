6099612ab3162d4dfb3a435ecdf9f625cee0caf7
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2023-04-11 19:53:19 +0900

    Add localization map

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
     use_respawn = LaunchConfiguration('use_respawn')
     log_level = LaunchConfiguration('log_level')
 
-    lifecycle_nodes = ['map_server', 'amcl']
+    lifecycle_nodes = ['navigation_map_server', 'localization_map_server', 'amcl']
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
     # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
@@ -54,8 +54,7 @@ def generate_launch_description():
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'yaml_filename': map_yaml_file}
+        'use_sim_time': use_sim_time,}
 
     configured_params = RewrittenYaml(
         source_file=params_file,
@@ -147,15 +146,25 @@ def generate_launch_description():
             ComposableNode(
                 package='nav2_map_server',
                 plugin='nav2_map_server::MapServer',
-                name='map_server',
+                name='navigation_map_server',
                 parameters=[configured_params],
-                remappings=remappings),
+                remappings = [('/tf', 'tf'),
+                              ('/tf_static', 'tf_static'),
+                              ('/map', 'navigation_map')]),
+            ComposableNode(
+                package='nav2_map_server',
+                plugin='nav2_map_server::MapServer',
+                name='localization_map_server',
+                parameters=[configured_params],
+                remappings = [('/tf', 'tf'),
+                              ('/tf_static', 'tf_static'),
+                              ('/map', 'localization_map')]),
             ComposableNode(
                 package='nav2_amcl',
                 plugin='nav2_amcl::AmclNode',
                 name='amcl',
                 parameters=[configured_params],
-                remappings=remappings),
+                remappings=[('/map', 'localization_map')]),
             ComposableNode(
                 package='nav2_lifecycle_manager',
                 plugin='nav2_lifecycle_manager::LifecycleManager',
